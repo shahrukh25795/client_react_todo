ff9a52a26d3e7a90e4bdd127cea7560f377ac6ca
Shahrukh Khan <shahrukh.rauf@jcdcsports.com>
2024-07-17 16:42:26 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <p>Welcome to srk house</p>
+    </div>
+  )
+}
+
+export default App
